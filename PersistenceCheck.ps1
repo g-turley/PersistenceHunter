214a523a904b7780f702aa39109f7183d64a44dd
yaboygmoney <goldstein.austin@gmail.com>
2020-03-11 14:47:34 -0500

    Update PersistenceCheck.ps1

--- PersistenceCheck.ps1
+++ PersistenceCheck.ps1
@@ -20,27 +20,55 @@ function Get-IFEO
 {
   if (Test-Path -path "Registry::HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\")
   {
-    Get-ChildItem -Path "Registry::HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\" |
-      where-object { $_.Property -like "*GlobalFlag*" }
+    Write-Host "[*] Checking Image Execution File Option keys.."
+    Write-Host ""
+    
+    keyList = Get-ChildItem -Path "Registry::HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\" |
+      where-object { $_.Property -like "*GlobalFlag*" } | Get-ItemProperty | Where-Object { $_.GlobalFlag -eq 512 } | 
+      Select -ExpandProperty PSPath | % {$_.split("::")[2] }
+    
+    foreach ($k in $keyList) {
+        Write-Host "Match found at $k"
+      }
+    Write-Host ""
+    Write-Host "[*] End of Image File Execution Options check"
   }
   
   else
   {
     Write-Host ""
-    Write-Host "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\ not found."
+    Write-Host "[*] HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\ not found"
+    Write-Host ""
   }
   
+  #SilentProcessExit Begin
   if (Test-Path -path "Registry::HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\")
   {
-    Get-ChildItem -Path "Registry::HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\" |
-      where-object { $_.Property -like "*MonitorProcess*" }
-    Get-ChildItem -Path "Registry::HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\" |
-      where-object { $_.Property -like "*ReportingMode*" }
-  }
+    Write-Host ""
+    Write-Host "[*] Checking SilentProcessExit keys.."
+    Write-Host ""
+    
+    $keyList = Get-ChildItem -Path "Registry::HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\" |
+      where-object { $_.Property -like "*MonitorProcess*" } | Get-ItemProperty | select @{l="Path";e={$_ | select -expandproperty PSPath |
+      % {$_.split("::")[2] }}}, @{l="BinaryLaunched";e={$_.MonitorProcess }}
+    
+    foreach ($k in $keyList)
+    {
+      $path = $k.path
+      $binary = $k.binaryLaunched
+      Write-Host "Match found at $path"
+      Write-Host "  Application launched: $binary"
+      Write-Host ""
+    }
+    
+    Write-Host "[*] End of SilentProcessExit check"
+    Write-Host ""    
+  } 
   
   else
   {
     Write-Host ""
     Write-Host "HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\ not found."
+    Write-Host ""
   }
 }
